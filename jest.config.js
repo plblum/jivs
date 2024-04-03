82f0d3c71e087af1ffedcf6d8c2dce42811f3816
Peter Blum <plblum@peterblum.com>
2024-04-03 17:12:15 -0400

    Fixing github actions

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,13 @@
 module.exports = {
-    // transform: {
-    //     "^.+\\.tsx?$": "ts-jest"
-    // },
-    // testRegex: "(/tests/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
-    // testPathIgnorePatterns: ["/packages/*/build/", "/node_modules/"],
-    // modulePaths: ["/packages/*/src/", "/examples/", "/starter_code/"],
-    // moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-    // collectCoverage: true,
-    // verbose: false,
-    // coveragePathIgnorePatterns: ["/examples/", "/starter_code"]
+    transform: {
+        "^.+\\.tsx?$": "ts-jest"
+    },
+    testRegex: "(/tests/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+    testPathIgnorePatterns: [
+        "/node_modules/", "/build/"],
+    modulePaths: ["/packages/*/src/"],
+    moduleFileExtensions: ["ts", "js" ],
+    collectCoverage: true,
+    verbose: false,
+    coveragePathIgnorePatterns: ["/packages/jivs-examples/", "/starter_code"]
 };
