04d9baa1b561ddf3f810cb895f405d6298e422f4
Peter Blum <plblum@peterblum.com>
2024-04-05 17:30:25 -0400

    Add a clean task to gulp for top level temp files

---
+++ gulpfile.mjs
@@ -0,0 +1,15 @@
+import gulp from 'gulp';
+import { deleteAsync as del } from 'del';
+
+// Define the 'clean' task
+gulp.task('clean', function () {
+    return del([
+        'typedoc_output/**/*',
+        'coverage/**/*',
+        'starter_code/build/**',
+        'packages/**/build/**/*',
+        'packages/**/src/**/*.d.ts',
+        // 'packages/**/src/**/*.js',
+         'packages/**/src/**/*.map'
+    ]);
+});
