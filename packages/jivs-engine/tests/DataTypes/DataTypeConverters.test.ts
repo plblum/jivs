65731ce605c3244156da4981dc2f5cc78a4b9f0c
Peter Blum <plblum@peterblum.com>
2024-04-06 11:24:03 -0400

    Change date converters to use total days

--- packages/jivs-engine/tests/DataTypes/DataTypeConverters.test.ts
+++ packages/jivs-engine/tests/DataTypes/DataTypeConverters.test.ts
@@ -3,8 +3,6 @@ import {
     TotalDaysConverter, IntegerConverter, TimeOfDayOnlyConverter, TimeOfDayHMSOnlyConverter
 } from './../../src/DataTypes/DataTypeConverters';
 import { LookupKey } from '../../src/DataTypes/LookupKeys';
-import { ComparersResult } from '../../src/Interfaces/DataTypeComparerService';
-import { DataTypeConverterService } from '../../src/Services/DataTypeConverterService';
 import { MockValidationServices } from '../Mocks';
 import { DataTypeComparerService } from '../../src/Services/DataTypeComparerService';
 describe('DataTypeConverter concrete classes', () => {
@@ -67,11 +65,26 @@ describe('DataTypeConverter concrete classes', () => {
             let testItem = new UTCDateOnlyConverter();
             // convert expects to be called after supportsValue is true.
             // So no illegal values as parameters tested
-            let test1 = new Date(Date.UTC(2000, 10, 5));
-            let test2 = new Date(Date.UTC(2023, 0, 1, 4, 30));
-            let test2dateonly = new Date(Date.UTC(2023, 0, 1));
-            expect(testItem.convert(test1, LookupKey.Date)).toBe(test1.getTime());
-            expect(testItem.convert(test2, LookupKey.Date)).toBe(test2dateonly.getTime());
+            let test1 = new Date(Date.UTC(1970, 0, 1));
+            let test2 = new Date(Date.UTC(1970, 0, 2, 4, 30));
+            let test3 = new Date(Date.UTC(1970, 0, 2));
+            let test4 = new Date(Date.UTC(1969, 11, 31));
+            let test5 = new Date(Date.UTC(1969, 11, 31, 23, 59, 59));
+            let test6 = new Date(Date.UTC(1971, 0, 1));            
+            let test7 = new Date(Date.UTC(1970, 11, 31));
+            let test8 = new Date(Date.UTC(1972, 2, 1)); // leap year, one day after leap day
+            let test9 = new Date(Date.UTC(1972, 1, 28)); // leap year, one day before leap day
+            let test10 = new Date(Date.UTC(1972, 1, 29)); // leap year, leap day
+            expect(testItem.convert(test1, LookupKey.Date)).toBe(0);
+            expect(testItem.convert(test2, LookupKey.Date)).toBe(1);
+            expect(testItem.convert(test3, LookupKey.Date)).toBe(1);           
+            expect(testItem.convert(test4, LookupKey.Date)).toBe(-1);   
+            expect(testItem.convert(test5, LookupKey.Date)).toBe(-1);           
+            expect(testItem.convert(test7, LookupKey.Date)).toBe(364);           
+            expect(testItem.convert(test6, LookupKey.Date)).toBe(365);           
+            expect(testItem.convert(test8, LookupKey.Date)).toBe(365 + 365 + 31 /* jan */ + 29 /* leap feb */);           
+            expect(testItem.convert(test9, LookupKey.Date)).toBe(365 + 365 + 31 /* jan */ + 27);           
+            expect(testItem.convert(test10, LookupKey.Date)).toBe(365 + 365 + 31 /* jan */ + 28);           
             // dates with an illegal value will convert to undefined
             let illegalDate = new Date("foo");
             expect(testItem.convert(illegalDate, LookupKey.Date)).toBeUndefined();
@@ -90,11 +103,28 @@ describe('DataTypeConverter concrete classes', () => {
             let testItem = new LocalDateOnlyConverter();
             // convert expects to be called after supportsValue is true.
             // So no illegal values as parameters tested
-            let test1 = new Date(2000, 10, 5);
-            let test2 = new Date(2023, 0, 1, 4, 30);
-            let test2dateonly = new Date(2023, 0, 1);
-            expect(testItem.convert(test1, LookupKey.LocalDate)).toBe(test1.getTime());
-            expect(testItem.convert(test2, LookupKey.LocalDate)).toBe(test2dateonly.getTime());
+            // Convert returns a total number of days since Jan 1, 1970, which equals 0.
+            // Thus the test data.
+            let test1 = new Date(1970, 0, 1);
+            let test2 = new Date(1970, 0, 2, 4, 30);
+            let test3 = new Date(1970, 0, 2);
+            let test4 = new Date(1969, 11, 31);
+            let test5 = new Date(1969, 11, 31, 23, 59, 59);
+            let test6 = new Date(1971, 0, 1);            
+            let test7 = new Date(1970, 11, 31);            
+            let test8 = new Date(1972, 2, 1); // leap year, one day after leap day
+            let test9 = new Date(1972, 1, 28); // leap year, one day before leap day
+            let test10 = new Date(1972, 1, 29); // leap year, leap day            
+            expect(testItem.convert(test1, LookupKey.LocalDate)).toBe(0);
+            expect(testItem.convert(test2, LookupKey.LocalDate)).toBe(1);
+            expect(testItem.convert(test3, LookupKey.LocalDate)).toBe(1);           
+            expect(testItem.convert(test4, LookupKey.LocalDate)).toBe(-1);   
+            expect(testItem.convert(test5, LookupKey.LocalDate)).toBe(-1);     
+            expect(testItem.convert(test7, LookupKey.LocalDate)).toBe(364);           
+            expect(testItem.convert(test6, LookupKey.LocalDate)).toBe(365);        
+            expect(testItem.convert(test8, LookupKey.LocalDate)).toBe(365 + 365 + 31 /* jan */ + 29 /* leap feb */);           
+            expect(testItem.convert(test9, LookupKey.LocalDate)).toBe(365 + 365 + 31 /* jan */ + 27);           
+            expect(testItem.convert(test10, LookupKey.LocalDate)).toBe(365 + 365 + 31 /* jan */ + 28);                 
             // dates with an illegal value will convert to undefined
             let illegalDate = new Date("foo");
             expect(testItem.convert(illegalDate, LookupKey.LocalDate)).toBeUndefined();
@@ -182,11 +212,26 @@ describe('DataTypeConverter concrete classes', () => {
             let testItem = new TotalDaysConverter();
             // convert expects to be called after supportsValue is true.
             // So no illegal values as parameters tested
-            let date1 = new Date(Date.UTC(2000, 10, 2));
-            let date2 = new Date(Date.UTC(2000, 10, 2, 5, 4, 3));         
-            let expected = Math.floor(date1.getTime() / 86400000);
-            expect(testItem.convert(date1, LookupKey.TotalDays)).toBe(expected);
-            expect(testItem.convert(date2, LookupKey.TotalDays)).toBe(expected);
+            let test1 = new Date(Date.UTC(1970, 0, 1));
+            let test2 = new Date(Date.UTC(1970, 0, 2, 4, 30));
+            let test3 = new Date(Date.UTC(1970, 0, 2));
+            let test4 = new Date(Date.UTC(1969, 11, 31));
+            let test5 = new Date(Date.UTC(1969, 11, 31, 23, 59, 59));
+            let test6 = new Date(Date.UTC(1971, 0, 1));            
+            let test7 = new Date(Date.UTC(1970, 11, 31));         
+            let test8 = new Date(Date.UTC(1972, 2, 1)); // leap year, one day after leap day
+            let test9 = new Date(Date.UTC(1972, 1, 28)); // leap year, one day before leap day
+            let test10 = new Date(Date.UTC(1972, 1, 29)); // leap year, leap day
+            expect(testItem.convert(test1, LookupKey.TotalDays)).toBe(0);
+            expect(testItem.convert(test2, LookupKey.TotalDays)).toBe(1);
+            expect(testItem.convert(test3, LookupKey.TotalDays)).toBe(1);           
+            expect(testItem.convert(test4, LookupKey.TotalDays)).toBe(-1);   
+            expect(testItem.convert(test5, LookupKey.TotalDays)).toBe(-1);      
+            expect(testItem.convert(test7, LookupKey.TotalDays)).toBe(364);           
+            expect(testItem.convert(test6, LookupKey.TotalDays)).toBe(365);    
+            expect(testItem.convert(test8, LookupKey.TotalDays)).toBe(365 + 365 + 31 /* jan */ + 29 /* leap feb */);           
+            expect(testItem.convert(test9, LookupKey.TotalDays)).toBe(365 + 365 + 31 /* jan */ + 27);           
+            expect(testItem.convert(test10, LookupKey.TotalDays)).toBe(365 + 365 + 31 /* jan */ + 28);                       
         });
     });    
 });
